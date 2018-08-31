e0454137b84abc13b65ace6f45d3abdebbe854c2
elisejakob <hei@elisejakob.no>
2018-08-31 09:58:31 +0200

    add text to intro page

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -12,9 +12,11 @@ function Intro({ close, data }) {
     <Primitives.Wizard>
       <IntroMain>
         <Primitives.Heading.H1>
-          Tilbygg - Bod, sykkelbod, vedbod, søppelskur
+          Produktdokumentasjon
         </Primitives.Heading.H1>
-        <Primitives.Paragraphs.P>Noe her</Primitives.Paragraphs.P>
+        <Primitives.Paragraphs.P>
+          Denne veiviseren gir informasjon til kommuner om dokumentasjon av 20 byggevarer. Ved å svare på en rekke spørsmål får kommunen svar på hva som kreves av dokumentasjon og hva som eventuelt mangler av dokumentasjon ved tilsyn.
+        </Primitives.Paragraphs.P>
         <Primitives.Button.MainButton type="button" onClick={() => close()}>
           Start veiviseren
         </Primitives.Button.MainButton>
