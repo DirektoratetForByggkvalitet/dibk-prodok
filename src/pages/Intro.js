a31b3322eeacb47b72600ed57535d53c1d9ccd20
elisejakob <hei@elisejakob.no>
2018-10-12 13:29:03 +0200

    update intro page text ++

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -14,13 +14,13 @@ function Intro({ close, data: { $computed, ...data } }) {
       <IntroMain>
         <Primitives.Heading.H1>Produktdokumentasjon</Primitives.Heading.H1>
         <Primitives.Paragraphs.Lead>
-          Denne veiviseren skal hjelpe deg som skal føre tilsyn på byggevarer. Du velger ut det produktet du skal undersøke og bestemmer deg på forhånd for hva du skal føre tilsyn med. Svar på veiviseren ut ifra det. Eksempel: produkt vinduer og tilsyn med brann.
+          Denne veiviseren skal hjelpe deg som skal føre tilsyn med hvordan en byggevare brukes i et konkret byggeprosjekt. Du velger ut det produktet du skal undersøke og bestemmer deg for hvilke deler av produktet du skal føre tilsyn med. Svar på veiviseren ut ifra det. Eksempel: produkt vinduer og tilsyn med brann.
         </Primitives.Paragraphs.Lead>
         <Primitives.Paragraphs.P>
-          Ved å svare på en rekke spørsmål får du svar på hva som kreves av dokumentasjon og hva som eventuelt mangler ved tilsyn. Du bestemmer selv hvor “dypt” du vil gå, og hvilke deler av produktet du vil føre tilsyn med. Du må ikke svare på alt om alt.
+          Ved å svare på en rekke spørsmål får du svar på hva som kreves av dokumentasjon og hva som eventuelt mangler. Du bestemmer selv hvor “dypt” du vil gå i tilsynet, og hvilke deler av produktet du vil føre tilsyn med. Du må ikke svare på alt om alt.
         </Primitives.Paragraphs.P>
         <Primitives.Paragraphs.P>
-          I veiviseren finner du 20 produkter. 13 av disse produktene har obligatorisk CE-merking og 7 har valgfri merking. Veiviseren er derfor ulik basert på hvilket produkt du velger og hvorvidt det er obligatorisk eller valgfri merking av produktet.
+          I veiviseren finner du 20 produkter. Ulike produkter har ulike krav til produktdokumentasjon. Dette henger blant annet sammen med om produktet er CE-merket eller ikke. Du vil derfor få ulike svaralternativer basert på hvilket produkt du velger.
         </Primitives.Paragraphs.P>
         <section>
           <div>
@@ -29,9 +29,9 @@ function Intro({ close, data: { $computed, ...data } }) {
               Bestem deg for:
             </Primitives.Paragraphs.P>
             <ol>
-              <li>hva slags bygning du skal føre tilsyn med</li>
-              <li>hvilket produkt i bygget du skal føre tilsyn med</li>
-              <li>hvilke egenskaper ved disse produktene du skal føre tilsyn med</li>
+              <li>hva slags byggverk eller tekniske installasjoner du skal føre tilsyn med.</li>
+              <li>hvilket produkt i byggverket eller installasjonen du skal føre tilsyn med.</li>
+              <li>hvilke egenskaper ved dette produktet du skal føre tilsyn med.</li>
             </ol>
             <Primitives.Paragraphs.P>
             Etter dette ber du om produktdokumentasjon og prosjekteringsgrunnlag fra ansvarlig søker. Når du har mottatt denne kan du starte veiviseren.
@@ -47,7 +47,7 @@ function Intro({ close, data: { $computed, ...data } }) {
         </Primitives.Paragraphs.P>
         <Primitives.Heading.H2>Når veiviseren er fullført</Primitives.Heading.H2>
         <Primitives.Paragraphs.P>
-        Når du har fullført veiviseren får du en resultatside som du kan bruke til å dokumentere tilsynet og arkivere.
+          Når du har fullført veiviseren får du en resultatside som du kan arkivere og bruke til å dokumentere tilsynet.
         </Primitives.Paragraphs.P>
         <Primitives.Button.MainButton type="button" onClick={() => close()}>
           Start veiviseren
